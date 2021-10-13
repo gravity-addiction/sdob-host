bb44bd8c96598729a94f93a70f3ceece681404e7
Gary Taylor <gary@strictdevelopment.com>
2021-10-13 06:56:47 -0700

    fix zero double prop fetch

--- server.c
+++ server.c
@@ -53,7 +53,7 @@ void *mpvTimerThread(void * arguments)
   char* lastMpvRet = NULL;
   while(!(*args->bCancel)) {
     char* mpvRet = mpv_get_property_string(args->mpvHandle, "time-pos");
-    if (mpvRet != NULL && strcmp(lastMpvRet, mpvRet) != 0) {
+    if (mpvRet != NULL) { // && strcmp(lastMpvRet, mpvRet) != 0) {
       printf("Time: %s\n", mpvRet);
       strlcpy(lastMpvRet, mpvRet, sizeof(mpvRet));
       s_send(timer, mpvRet);
